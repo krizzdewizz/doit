cc88d5cb49021e9fa92b57a618c0072c4361d889
Christian Oetterli <christian.oetterli@bison-group.com>
2016-12-24 13:11:32 +0100

    improve

--- server/app/vars.ts
+++ server/app/vars.ts
@@ -1,14 +1,16 @@
 import { Vars } from './model';
 
 export class Varss {
+    private regExs: { [v: string]: RegExp } = {};
+
     constructor(private vars: Vars) {
+        Object.keys(this.vars).forEach(v => this.regExs[v] = new RegExp('\\$\\{(' + v + ')\\}', 'g'));
     }
 
     replace(s: string): string {
-        if (!s) {
-            return s;
+        if (s) {
+            Object.keys(this.vars).forEach(v => s = s.replace(this.regExs[v], this.vars[v]));
         }
-        Object.keys(this.vars).forEach(k => s = s.replace(new RegExp('\\$\\{(' + k + ')\\}', 'g'), this.vars[k]));
         return s;
     }
 }
